d5fcf0d12a2cc35cf91abca42bbd6a9d6f0dea32
garrett <yien.garrett94@gmail.com>
2021-08-05 14:52:47 -0400

    struggling to figure out async for last 2 tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import {render, screen, waitFor } from '@testing-library/react';
+import {getByTestId, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import App from '../App';
 import ContactForm from './ContactForm';
 
 test('renders without errors', ()=>{
@@ -61,17 +60,59 @@ test('renders ONE error message if user enters a valid first name and last name 
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+    render(<ContactForm/>);
+    const placeHolderEmail = 'bluebill1049@hotmail.com';
+    const emailInput = screen.getByPlaceholderText(placeHolderEmail);
+    const inputTextEmail = 'notavalidemail';
+    userEvent.type(emailInput, inputTextEmail);
+    const errorTextEmail = 'email must be a valid email address.';
+    expect(errorTextEmail).toBeInTheDocument;
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+    render(<ContactForm/>);
+    const placeHolderLast = 'Burke'
+    const lastNameInput = screen.getByPlaceholderText(placeHolderLast);
+    userEvent.type(lastNameInput, '{enter}');
+    const errorTextLast = 'lastName is a required field.';
+    expect(errorTextLast).toBeInTheDocument;
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
+    render(<ContactForm/>);
+
+    const placeHolderFirst = 'Edd';
+    const firstNameInput = screen.getByPlaceholderText(placeHolderFirst);
+    const inputTextFirst = 'abcde';
+    userEvent.type(firstNameInput, inputTextFirst);
+
+    const placeHolderLast = 'Burke'
+    const lastNameInput = screen.getByPlaceholderText(placeHolderLast);
+    const inputTextLast = 'a';
+    userEvent.type(lastNameInput, inputTextLast);
+
+    const placeHolderEmail = 'bluebill1049@hotmail.com';
+    const emailInput = screen.getByPlaceholderText(placeHolderEmail);
+    const inputTextEmail = 'avalid@email.com';
+    userEvent.type(emailInput, inputTextEmail);
+    userEvent.type(emailInput, '{enter}');
     
+    
+    const firstnameDisplay = await screen.findByTestId('firstnameDisplay');
+    const lastnameDisplay = await screen.findByTestId('lastnameDisplay');
+    const emailDisplay = await screen.findByTestId('emailDisplay');
+    // const messageDisplay = getByTestId('messageDisplay');
+
+    await waitFor(() => expect(findByTestId(firstnameDisplay)).toBeInTheDocument);
+    await waitFor(() => expect(findByTestId(lastnameDisplay)).toBeInTheDocument);
+    await waitFor(() => expect(findByTestId(emailDisplay)).toBeInTheDocument);
+    
+    // expect(firstnameDisplay).toHaveTextContent(inputTextFirst); 
+    // expect(lastnameDisplay).toHaveTextContent(inputTextLast);
+    // expect(emailDisplay).toHaveTextContent(inputTextEmail);
+    // expect(findByTestId('messageDisplay')).not.toBeInTheDocument;
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
-    
+    render(<ContactForm/>);
 });
